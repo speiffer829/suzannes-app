af5d9c257aad6848209ac0ff7f1366a4a9295c17
Spenser <spenser@minddnd.com>
2023-01-29 15:15:22 -0500

    used layout better for login/register page

---
+++ src/routes/(unprotected)/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+</script>
+
+<div class="grid lg:grid-cols-5 md:grid-cols-2">
+	<img
+		src={`/unprotected-imgs/${data.img}.png`}
+		alt="painting of gymnast"
+		class="lg:col-span-3 block md:min-h-screen h-36 w-full max-w-full object-cover"
+	/>
+	<div class="lg:col-span-2 flex justify-center align-center">
+		<slot />
+	</div>
+</div>
